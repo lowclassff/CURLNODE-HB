ee5597580d037c5719a25426f61ab2b5c9efd2a7
lowclassff <lowclass796@gmail.com>
2025-08-13 20:44:04 +0545

    Create hb.sh

---
+++ hb.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# A simple heartbeat script.
+# You can replace this with a real curl command to a service.
+
+while true
+do
+  echo "CURLNODE HB is alive at $(date)"
+  sleep 10
+done
